c487c73e5af52909e51ccc0703c26c80c7638798
alaminislamrahat <alaminislamrahat@gmail.com>
2023-09-18 22:40:29 +0600

    ddff

---
+++ object.js
@@ -0,0 +1,13 @@
+const student = [
+    {id : 45, name : "rahat"},
+    {id : 44, name : "alamin"},
+    {id : 52, name : "lemon"},
+    {id : 88, name : "tufan"},
+];
+
+
+const result = student.map( s => s.name)
+console.log(result);
+
+
+
